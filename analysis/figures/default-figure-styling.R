723d24fc2139cf55ff954798053340e294e56494
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2023-09-09 12:15:49 -0600

    added bold text to default theme

--- analysis/figures/default-figure-styling.R
+++ analysis/figures/default-figure-styling.R
@@ -3,8 +3,9 @@ library('stringi') # for working with strings
 
 # set default ggplot theme
 # cannot bold text because unicode characters fail
-theme_set(theme_bw() + theme(legend.position = 'none',
-                             text = element_text(size = 15)))
+theme_set(theme_bw() +
+            theme(legend.position = 'none',
+                  text = element_text(size = 15, face = 'bold')))
 
 # custom color-blind palette
 pal <- c('#ff8c00', '#4477AA', '#009900', '#66CCEE',
@@ -14,3 +15,8 @@ pal <- c('#ff8c00', '#4477AA', '#009900', '#66CCEE',
 create_ndvi_pal <- colorRampPalette(c('darkblue', 'dodgerblue', '#744700', '#d9bb94',
                                       'darkgreen'))
 ndvi_pal <- create_ndvi_pal(100)
+
+# mean and variance notations
+e_r <- expression(E(italic(R)))
+var_r <- expression(Var(italic(R)))
+h <- 'Space-use requirements'
