f5f84de3f440a2a012dd09fe120b7ebdc15206a8
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2024-02-02 09:19:04 -0800

    added ti() panel

--- analysis/simulations/6-modeling-R-and-hr.R
+++ analysis/simulations/6-modeling-R-and-hr.R
@@ -4,6 +4,10 @@ library('mgcv')    # for empirical Bayes GAMs
 library('ggplot2') # for fancy plots
 library('cowplot') # for fancy multi-panel plots
 source('analysis/figures/default-figure-styling.R') # for color palette
+theme_set(theme_get() +
+            theme(legend.position = 'none',
+                  strip.background = element_blank(),
+                  strip.text = element_text(size = 14, face = 'bold')))
 
 e_r <- 'paste(bold("Resource abundance, E("), bolditalic("R"), bold(")"))'
 v_r <- 'paste(bold("Resource stochasticity, Var("), bolditalic("R"), bold(")"))'
@@ -68,7 +72,7 @@ plot(m, all.terms = TRUE, pages = 1, scheme = 3, scale = 0)
 newd_mu <-
   expand_grid(mu = seq(min(sims$mu), max(sims$mu), length.out = 400),
               sigma2 = mean(sims$sigma2),
-              quantile = unique(sims$quantile))
+              quantile = 'hr_95')
 
 preds_mu <-
   bind_cols( # bind new data and predictions
@@ -80,13 +84,25 @@ preds_mu <-
                 hr_lwr = exp(fit.1 - se.fit.1 * 1.96),
                 hr_upr = exp(fit.1 + se.fit.1 * 1.96)))
 
+p_mu <-
+  ggplot() +
+  geom_point(aes(mu, hr), filter(sims, quantile == 'hr_95'),
+             alpha = 0.1, color = pal[3]) +
+  geom_ribbon(aes(mu, ymin = hr_lwr, ymax = hr_upr), fill = pal[1],
+              preds_mu, alpha = 0.5) +
+  geom_line(aes(mu, hr), color = pal[1], linewidth = 1, preds_mu) +
+  scale_x_continuous(
+    bquote(paste(bold('Resource abundance, E('),
+                 bolditalic('R'), bold(')'))), breaks = NULL) +
+  scale_y_continuous(bquote(paste(bold('Space-use requirements, '),
+                                  bolditalic('H'))), breaks = NULL)
 
 # effect of sigma2
 newd_sigma2 <-
   expand_grid(mu = mean(sims$mu),
               sigma2 = seq(min(sims$sigma2), max(sims$sigma2),
                            length.out = 400),
-              quantile = unique(sims$quantile))
+              quantile = 'hr_95')
 
 preds_sigma2 <-
   bind_cols(
@@ -97,39 +113,61 @@ preds_sigma2 <-
                 hr_lwr = exp(fit.1 - se.fit.1 * 1.96),
                 hr_upr = exp(fit.1 + se.fit.1 * 1.96)))
 
-# final figure
-preds <- bind_rows(mutate(preds_mu, x = e_r) %>%
-                     rename(value = mu) %>%
-                     select(-sigma2),
-                   mutate(preds_sigma2, x = v_r) %>%
-                     rename(value = sigma2) %>%
-                     select(-mu))
-
-sims_l <- sims %>%
-  mutate(e = hr - predict(m, newdata = sims, type = 'response')[, 1]) %>%
-  pivot_longer(c(mu, sigma2)) %>%
-  mutate(x = if_else(name == 'mu', e_r, v_r))
-
-ggplot() +
-  facet_grid(. ~ x, scales = 'free', switch = 'x',
-             labeller = label_parsed) + # label below x axis
-  geom_point(aes(value, hr), filter(sims_l, quantile == 'hr_95'),
+p_sigma2 <-
+  ggplot() +
+  geom_point(aes(sigma2, hr), filter(sims, quantile == 'hr_95'),
              alpha = 0.1, color = pal[3]) +
-  geom_ribbon(aes(value, ymin = hr_lwr, ymax = hr_upr, fill = x),
-              filter(preds, quantile == 'hr_95'), alpha = 0.5) +
-  geom_line(aes(value, hr, color = x), linewidth = 1,
-            filter(preds, quantile == 'hr_95')) +
-  scale_color_manual(values = pal) +
-  scale_x_continuous(NULL, breaks = NULL) + 
+  geom_ribbon(aes(sigma2, ymin = hr_lwr, ymax = hr_upr), fill = pal[2],
+              preds_sigma2, alpha = 0.5) +
+  geom_line(aes(sigma2, hr), color = pal[2], linewidth = 1,
+            preds_sigma2) +
+  scale_x_continuous(
+    bquote(paste(bold('Resource stochasticity, Var('),
+                 bolditalic('R'), bold(')'))), breaks = NULL) +
   scale_y_continuous(bquote(paste(bold('Space-use requirements, '),
-                                  bolditalic('H'))), breaks = NULL) +
-  # to make facet strip look like the x axis
-  theme(legend.position = 'none',
-        strip.background = element_blank(),
-        strip.text = element_text(size = 14, face = 'bold'))
+                                  bolditalic('H'))), breaks = NULL)
+
+# full interaction plot
+newd_mu_sigma2 <-
+  expand_grid(mu = seq(min(sims$mu), max(sims$mu), length.out = 400),
+              sigma2 = seq(min(sims$sigma2), max(sims$sigma2),
+                           length.out = 400),
+              quantile = 'hr_95')
+
+preds_mu_sigma2 <-
+  bind_cols(newd_mu_sigma2,
+            predict(m, newdata = newd_mu_sigma2, type = 'response',
+                    se.fit = TRUE, terms = c('ti(mu,sigma2)',
+                                             'ti(mu,sigma2).1')) %>%
+              as.data.frame()) %>%
+  rename(hr = fit.1)
+
+p_mu_sigma2 <-
+  ggplot(preds_mu_sigma2) +
+  geom_raster(aes(mu, sigma2, fill = hr)) +
+  geom_contour(aes(mu, sigma2, z = hr), color = 'black') +
+  scale_x_continuous(
+    bquote(paste(bold('Resource abundance, E('),
+                 bolditalic('R'), bold(')'))), breaks = NULL,
+    expand = c(0, 0)) +
+  scale_y_continuous(
+    bquote(paste(bold('Resource stochasticity, Var('),
+                 bolditalic('R'), bold(')'))), breaks = NULL,
+    expand = c(0, 0)) +
+  scale_fill_gradient(bquote(paste(atop(bold('Space-use'),
+                                        paste(bold('requirements, '),
+                                              bolditalic('H  '))))),
+                      breaks = range(preds_mu_sigma2$hr),
+                      labels = c('Low', 'High'),
+                      low = 'grey90', high = pal[3]) +
+  theme(legend.position = 'top')
+
+# final figure
+cowplot::plot_grid(p_mu, p_sigma2, p_mu_sigma2, labels = 'AUTO',
+                   nrow = 1)
 
 ggsave('figures/simulation-regression-plots.png',
-       width = 6, height = 3, dpi = 'print', bg = 'white', scale = 1.5)
+       width = 9, height = 3, dpi = 'print', bg = 'white', scale = 1.5)
 
 # effect of sqrt(sigma2) is nonlinear since it is on the same scale as mu
 sd_r <- 'paste(bold("Standard deviation in "), bolditalic("R"))'
