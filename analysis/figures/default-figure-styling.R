dcd615bc468a70bac810d30a6d2ebe8c5ec1c44d
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2024-05-29 18:30:16 -0600

    made color palettes colorblind-friendly

--- analysis/figures/default-figure-styling.R
+++ analysis/figures/default-figure-styling.R
@@ -1,4 +1,7 @@
 library('ggplot2') # for fancy figures
+library('khroma')  # for color palettes
+
+.check_colors <- FALSE
 
 # set default ggplot theme
 theme_set(theme_bw() +
@@ -6,16 +9,18 @@ theme_set(theme_bw() +
                   text = element_text(size = 15, face = 'bold'),
                   panel.grid = element_blank()))
 
-# custom color-blind palette
-pal <- c('#ff8c00', '#4477AA', '#009900', '#66CCEE',
-         '#CCBB44', '#EE6677', '#AA3377', '#BBBBBB')
+# custom color-blind-friendly palette
+pal <- c('#F09D00', '#3388E1', '#1C7734', '#992266')
+
+if(.check_colors) plot_scheme_colorblind(pal)
 
 # custom NDVI color palette
-create_ndvi_pal <- colorRampPalette(c('darkblue', 'dodgerblue', '#744700', '#d9bb94',
-                                      'darkgreen'))
+create_ndvi_pal <- colorRampPalette(c('darkblue', 'dodgerblue', '#744700',
+                                      '#d9bb94', 'darkgreen'))
 ndvi_pal <- create_ndvi_pal(100)
+if(.check_colors) plot_scheme_colorblind(ndvi_pal)
 
 # mean and variance notations
 e_r <- bquote(paste(bold('E('), bolditalic('R'), bold(')')))
 var_r <- bquote(paste(bold('Var('), bolditalic('R'), bold(')')))
-h <- bquote(paste(bold('Space-use requirements, '), bolditalic('H')))
+h <- bquote(paste(bold('Home-range size, '), bolditalic('H')))
